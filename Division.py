0617b025cddd2bb13730634a7c875f574bfe7c92
eMobilis <carmenmose2004@gmail.com>
2023-02-06 12:43:57 +0300

    added sme functions

---
+++ Division.py
@@ -0,0 +1,4 @@
+num1 = 50
+num2 = 100
+divide = num2/num1
+print(divide)
--- second.py
+++ second.py
@@ -1,4 +1,8 @@
 Firstname = "Marcos"
 Secondname = "Hernandez"
+Car 1 = "Range Rover"
+car 2 = "Mercedes-Benz"
+
+
 
 print(Firstname+Secondname)
