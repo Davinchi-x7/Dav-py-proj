80e66fa337595700f862bdf57cea73f443afbbfb
eMobilis <carmenmose2004@gmail.com>
2023-02-09 12:18:44 +0300

    if statements

---
+++ Conditions/if statements.py
@@ -0,0 +1,25 @@
+number = int(input("Ener pompt commnad:"))
+if number == 544:
+    print("Select desired plan:")
+    print("1: Daily bundles")
+    print("2: Data deals")
+    print("3: Tunukiwa")
+    print("4: fuliza")
+    print("5: Mpesa")
+
+else:
+    print("invalid entry please try again")
+
+num = int(input("Select plan:"))
+if num == 1:
+    print("You have selected daily bundles")
+elif num == 2:
+    print("You have selected data deals")
+elif num == 3:
+    print("You have selected Tunukiwa")
+elif num == 4:
+    print("You have selected fuliza")
+elif num == 5:
+    print("You have selected Mpesa")
+else:
+    print("invalid entry please try again")
--- Counter.py
+++ Counter.py
@@ -2,9 +2,9 @@ import datetime
 x = datetime.datetime.now()
 wday = x.strftime("%j")
 day = int(wday)
-print("this is the", day, "th day of the year")
+print(f"this is the{day} th day of the year")
 
 if day < 365:
-    print("Total to save today is KSH", day)
+    print(f"Total to save today is KSH{day}")
 
 
