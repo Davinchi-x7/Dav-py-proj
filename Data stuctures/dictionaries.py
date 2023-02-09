3fff69e1480cb3f8938298c84acb606cfd16f784
eMobilis <carmenmose2004@gmail.com>
2023-02-09 11:40:24 +0300

    dictionaries

---
+++ Data stuctures/dictionaries.py
@@ -0,0 +1,6 @@
+# dictionaries use {}
+# dictionaries have a key assigned to the name sort of like an index
+
+fruits = {"Apple":5, "mango":10, "banana":6, "kiwi":8, "avocado":10}
+fruits["peach"] = 2
+print(fruits)
--- Data stuctures/turple.py
+++ Data stuctures/turple.py
@@ -1,7 +1,7 @@
 #turple is defined by a normal bracket
 # turple will also keep order
 # you cant modify/add data in a turple
-# you can find position of a particular item in turples
+# you can find position of a particular item in turples using []
 
 myturple = ("Ezra", "Peter", "Wendy", "Vanessa")
 
