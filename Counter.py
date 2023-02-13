62b1c9a8257015d23fcba353f989c6aa5dc1b51f
eMobilis <carmenmose2004@gmail.com>
2023-02-13 11:41:07 +0300

    funcions

--- Counter.py
+++ Counter.py
@@ -2,9 +2,13 @@ import datetime
 x = datetime.datetime.now()
 wday = x.strftime("%j")
 day = int(wday)
-print(f"this is the{day} th day of the year")
+print(f"this is the {day}th day of the year")
+
+
 
 if day < 365:
-    print(f"Total to save today is KSH{day}")
 
+    print(f"Total to save today is KSH {day}")
+
+    #print(f"Total savings till today is KSH{SAV}")
 
--- sentence checker.py
+++ sentence checker.py
@@ -1,2 +1,4 @@
-word = input("Enter a sentence:")
-print(len(word))
+def calc():
+    day = int(input("Enter day:"))
+    SAV = day / 2*((2 * day) + (day - 1) * 1)
+    print(f"Total is: {SAV}")
