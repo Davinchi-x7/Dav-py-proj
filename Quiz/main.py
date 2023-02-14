56bc8bb1137995dd8f553337ab04c81315e8b1d3
Davinchi-x7 <dufgvjdu@gmail.com>
2023-02-14 12:14:49 +0300

    updated for loop quiz

--- Quiz/main.py
+++ Quiz/main.py
@@ -1,8 +1,9 @@
-for number in range(1, 100):
-    if number/3:
-        print("Fizz")
-    if number/3 and 5:
-        print("FizzBuzz")
-    if number/5:
-        print("Buzz")
 
+
+for number in range(1,101):
+    if(number%3==0 and number%5==0):
+        print("FizzBuzz")
+    elif(number%3 ==0):
+        print("Fizz")
+    elif(number%5==0):
+        print("buzz")
