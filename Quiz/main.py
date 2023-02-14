43058ce63fcbe2f2a0e598aa290cbfb44ab5a2a9
Davinchi-x7 <dufgvjdu@gmail.com>
2023-02-14 12:47:01 +0300

    giving dynamic range

--- Quiz/main.py
+++ Quiz/main.py
@@ -1,9 +1,12 @@
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter Second Number:"))
 
-
-for number in range(1,101):
+for number in range(num1,num2):
     if(number%3==0 and number%5==0):
-        print("FizzBuzz")
+        print(f"{number}:FizzBuzz")
     elif(number%3 ==0):
-        print("Fizz")
+        print(f"{number}:Fizz")
     elif(number%5==0):
-        print("buzz")
+        print(f"{number}buzz")
+    else:
+        print(number)
