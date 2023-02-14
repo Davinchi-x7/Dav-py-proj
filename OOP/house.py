02cb5de3ed307e25de1b63ac29c47b3657b54f86
Davinchi-x7 <dufgvjdu@gmail.com>
2023-02-14 13:32:23 +0300

    advanced classes OOP

--- OOP/house.py
+++ OOP/house.py
@@ -1,17 +1,36 @@
-class House():
-    def __init__(self, type, location, owner):
-        self.type = type
-        self.location = location
-        self.owner = owner
+class Car():
+    def __init__(self, brand, model,body, drivetrain):
+        self.brand = brand
+        self.model = model
+        self.body = body
+        self.drivetrain = drivetrain
 
-owner_one = House("Bungalow", "Kitengela", "Sharon")
-print(owner_one.type)
-print(owner_one.location)
-print(owner_one.owner)
+owner_one = Car("Mercedes-Benz", "S-class 600", "sedan", "AWD")
+print(owner_one.brand)
+print(owner_one.model)
+print(owner_one.body)
+print(owner_one.drivetrain)
 
 print("----------End of owner 1------------")
 
-owner_two = House("Mansion", "Lavington", "Paul")
-print(owner_two.type)
-print(owner_two.location)
-print(owner_two.owner)
+owner_two = Car("BMW", "X7", "SUV", "4WD")
+print(owner_two.brand)
+print(owner_two.model)
+print(owner_two.body)
+print(owner_two.drivetrain)
+
+print("---End of owner 2---")
+
+owner_three = Car("Audi", "A8", "Sedan", "AWD")
+print(owner_three.brand)
+print(owner_three.model)
+print(owner_three.body)
+print(owner_three.drivetrain)
+
+print("---End of owner 3---")
+
+owner_four = Car("Volkswagen", "Touareg", "SUV", "4WD")
+print(owner_four.brand)
+print(owner_four.model)
+print(owner_four.body)
+print(owner_four.drivetrain)
